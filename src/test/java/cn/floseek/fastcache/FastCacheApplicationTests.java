562dff1ce461978356ab1472bbda5a7de24f364f
ChenHongwei472 <281339961@qq.com>
2025-08-11 15:08:48 +0800

    feat: 初始化项目

---
+++ src/test/java/cn/floseek/fastcache/FastCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.floseek.fastcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
