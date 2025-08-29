42c8d83348ed2311f022b25dfa74e98ae1e8b1c5
ChenHongwei472 <281339961@qq.com>
2025-08-29 22:59:51 +0800

    refactor: 移除 RedisService 接口及相关实现

--- fast-cache-starters/fast-cache-redisson-spring-boot-starter/src/main/java/cn/floseek/fastcache/config/RedissonAutoConfiguration.java
+++ fast-cache-starters/fast-cache-redisson-spring-boot-starter/src/main/java/cn/floseek/fastcache/config/RedissonAutoConfiguration.java
@@ -4,11 +4,9 @@ import cn.floseek.fastcache.bloomfilter.BloomFilterFactory;
 import cn.floseek.fastcache.cache.builder.RemoteCacheBuilder;
 import cn.floseek.fastcache.config.properties.FastCacheProperties;
 import cn.floseek.fastcache.lock.LockTemplate;
-import cn.floseek.fastcache.redis.RedisService;
 import cn.floseek.fastcache.redisson.RedissonBloomFilterFactory;
 import cn.floseek.fastcache.redisson.RedissonCacheBuilder;
 import cn.floseek.fastcache.redisson.RedissonLockTemplate;
-import cn.floseek.fastcache.redisson.RedissonService;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -24,11 +22,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(FastCacheProperties.class)
 public class RedissonAutoConfiguration {
-
-    @Bean
-    public RedisService redisService(RedissonClient redissonClient) {
-        return new RedissonService(redissonClient);
-    }
 
     @Bean
     public BloomFilterFactory redissonBloomFilterFactory(RedissonClient redissonClient) {
